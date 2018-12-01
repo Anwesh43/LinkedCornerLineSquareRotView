230e3c0e3e2c219cf074c903b108456b45d5ef46
Anwesh43 <anweshthecool0@gmail.com>
2018-12-01 10:48:48 +0530

    adding CLSRNode class

--- cornerlinesquarerotview/src/main/java/com/anwesh/uiprojects/cornerlinesquarerotview/CornerLineSquareRotView.kt
+++ cornerlinesquarerotview/src/main/java/com/anwesh/uiprojects/cornerlinesquarerotview/CornerLineSquareRotView.kt
@@ -118,8 +118,53 @@ class CornerLineSquareRotView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated ) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class CLSRNode(var i : Int, val state : State = State()) {
+
+        private var prev : CLSRNode? = null
+
+        private var next : CLSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CLSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCLSRNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CLSRNode {
+            var curr : CLSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
