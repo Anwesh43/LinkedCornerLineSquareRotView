e9e3c4629ceac818f1017c34e731c36c651d1466
Anwesh43 <anweshthecool0@gmail.com>
2018-12-01 10:16:45 +0530

    adding libraries and defining nodes

--- cornerlinesquarerotview/src/main/java/com/anwesh/uiprojects/cornerlinesquarerotview/CornerLineSquareRotView.kt
+++ cornerlinesquarerotview/src/main/java/com/anwesh/uiprojects/cornerlinesquarerotview/CornerLineSquareRotView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.cornerlinesquarerotview
 /**
  * Created by anweshmishra on 01/12/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val color : Int = Color.parseColor("#00C853")
+val sizeFactor : Int = 3
+val strokeFactor : Int = 90
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
