ae608cf907f61ab7f001787866eb5f04277852cb
Anwesh43 <anweshthecool0@gmail.com>
2018-12-01 11:12:01 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedcornerlinesquarerotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcornerlinesquarerotview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedcornerlinesquarerotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.cornerlinesquarerotview.CornerLineSquareRotView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         CornerLineSquareRotView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
