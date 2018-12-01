c1f0846c02fd971d13f5209bf45cfb39de6459e6
Anwesh43 <anweshthecool0@gmail.com>
2018-12-01 10:26:39 +0530

    adding drawCLSRNode canvas extension method

--- cornerlinesquarerotview/src/main/java/com/anwesh/uiprojects/cornerlinesquarerotview/CornerLineSquareRotView.kt
+++ cornerlinesquarerotview/src/main/java/com/anwesh/uiprojects/cornerlinesquarerotview/CornerLineSquareRotView.kt
@@ -29,3 +29,32 @@ fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInv
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
+fun Canvas.drawCLSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(0, 2)
+    val size : Float = gap / sizeFactor
+    paint.color = color
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(90f * sc2)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        save()
+        rotate(90f * j)
+        for (k in 0..1) {
+            save()
+            translate(size, size)
+            rotate(90f * k)
+            drawLine(0f, 0f, -(size/3) * sc, 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
